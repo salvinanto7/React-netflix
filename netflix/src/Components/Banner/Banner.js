6d156dfd7cbad8c1bb7ba3d8d49ba18b08777cf0
salvinanto7 <salvinanto7@gmail.com>
2022-10-04 13:01:42 +0530

    banner section automated

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../Constants/constants'
 function Banner() {
+  const [movie,setMovie]= useState()
+  useEffect(()=>{
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data)
+      setMovie(response.data.results[Math.floor(Math.random() * 20)])
+    })
+  },[])
   return (
-    <div className='banner'>
+    <div className='banner' style={{backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:""})`}}>
       <div className='content'>
-        <h1 className='title'>Movie Name</h1>
+        <h1 className='title'>{movie?movie.title:""}</h1>
         <div className='banner_buttons'>
             <button className='button'>Play</button>
             <button className='button'>My List</button>
         </div>
-        <h2 className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </h2>
+        <h2 className='description'>{movie?movie.overview:""}</h2>
       </div>
       <div className="fade_bottom"></div>
     </div>
---
+++ netflix/src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'ff57d35b37e2178753bf56daa4ff9147';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
