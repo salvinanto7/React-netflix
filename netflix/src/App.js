5f8d33d970dee99117d351f4eec872a074752780
salvinanto7 <salvinanto7@gmail.com>
2022-10-05 09:53:12 +0530

    added few more section

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 import Rowpost from './Components/Rowpost/Rowpost';
-import {ACTION,ORIGINALS,COMEDY,HORROR} from './urls'
+import {ACTION,ORIGINALS,COMEDY,HORROR,ROMANCE,DOCUMENTARIES} from './urls'
 function App() {
   return (
     <div className="App">
@@ -13,6 +13,8 @@ function App() {
       <Rowpost url={ACTION} title='Action' isSmall />
       <Rowpost url={COMEDY} title='Comedy' isSmall />
       <Rowpost url={HORROR} title='Horror' isSmall />
+      <Rowpost url={ROMANCE} title='Romance' isSmall />
+      <Rowpost url={DOCUMENTARIES} title='Documentaries' isSmall />
     </div>
   );
 }
--- netflix/src/urls.js
+++ netflix/src/urls.js
@@ -3,3 +3,5 @@ export const ACTION = `discover/movie?api_key=${API_KEY}&with_genres=28`
 export const ORIGINALS = `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const COMEDY = `discover/movie?api_key=${API_KEY}&with_genres=35`
 export const HORROR =`discover/movie?api_key=${API_KEY}&with_genres=27`
+export const ROMANCE = `discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const DOCUMENTARIES =`discover/movie?api_key=${API_KEY}&with_genres=99`
