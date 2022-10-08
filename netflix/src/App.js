3b121c28bedc1e8ccf5a023e7bc5ebff89960f41
salvinanto7 <salvinanto7@gmail.com>
2022-10-08 13:17:13 +0530

    added more sections to page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,18 +3,54 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 import Rowpost from './Components/Rowpost/Rowpost';
-import {ACTION,ORIGINALS,COMEDY,HORROR,ROMANCE,DOCUMENTARIES} from './urls'
+import {
+  ACTION_MOVIES,
+  ORIGINALS,
+  COMEDY_MOVIES,
+  HORROR_MOVIES,
+  ROMANCE_MOVIES,
+  THRILLER_MOVIES,
+  SCIFI_MOVIES,
+  DOCUMENTARIES_MOVIES,
+  ACTION_ADVENTURE_SERIES,
+  COMEDY_SERIES,
+  ROMANCE_SERIES,
+  TALK,
+  KIDS_SERIES,
+  ANIMATION_SERIES,
+  SCIFI_SERIES,
+  MYSTERY_SERIES,
+  CRIME_SERIES,
+  DOCUMENTARIES_SERIES} from './urls'
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
       <Rowpost url={ORIGINALS}  title='Netflix Original'/>
-      <Rowpost url={ACTION} title='Action' isSmall />
-      <Rowpost url={COMEDY} title='Comedy' isSmall />
-      <Rowpost url={HORROR} title='Horror' isSmall />
-      <Rowpost url={ROMANCE} title='Romance' isSmall />
-      <Rowpost url={DOCUMENTARIES} title='Documentaries' isSmall />
+      <div className='section-heading'>
+      <h2>Movies</h2>
+      </div>
+      <Rowpost url={ACTION_MOVIES} title='Action' isSmall />
+      <Rowpost url={COMEDY_MOVIES} title='Comedy' isSmall />
+      <Rowpost url={HORROR_MOVIES} title='Horror' isSmall />
+      <Rowpost url={THRILLER_MOVIES} title='Thriller' isSmall />
+      <Rowpost url={SCIFI_MOVIES} title='Science Fiction' isSmall />
+      <Rowpost url={ROMANCE_MOVIES} title='Romance' isSmall />
+      <Rowpost url={DOCUMENTARIES_MOVIES} title='Documentaries' isSmall />
+      <div className='section-heading'>
+      <h2>TV Series</h2>
+      </div>
+      <Rowpost url={ACTION_ADVENTURE_SERIES} title="Action & Adventure" isSmall />
+      <Rowpost url={SCIFI_SERIES} title='Science Fiction' isSmall />
+      <Rowpost url={MYSTERY_SERIES} title='Mystery' isSmall />
+      <Rowpost url={CRIME_SERIES} title='Crime' isSmall />
+      <Rowpost url={KIDS_SERIES} title='Kids' isSmall />
+      <Rowpost url={ANIMATION_SERIES} title='Animation' isSmall />
+      <Rowpost url={COMEDY_SERIES} title='Comedy' isSmall />
+      <Rowpost url={ROMANCE_SERIES} title='Romance' isSmall />
+      <Rowpost url={TALK} title='Romance' isSmall />
+      <Rowpost url={DOCUMENTARIES_SERIES} title='Documentaries' isSmall />
     </div>
   );
 }
--- netflix/src/urls.js
+++ netflix/src/urls.js
@@ -1,7 +1,23 @@
 import {API_KEY} from './Constants/constants'
-export const ACTION = `discover/movie?api_key=${API_KEY}&with_genres=28`
 export const ORIGINALS = `discover/tv?api_key=${API_KEY}&with_networks=213`
-export const COMEDY = `discover/movie?api_key=${API_KEY}&with_genres=35`
-export const HORROR =`discover/movie?api_key=${API_KEY}&with_genres=27`
-export const ROMANCE = `discover/movie?api_key=${API_KEY}&with_genres=10749`
-export const DOCUMENTARIES =`discover/movie?api_key=${API_KEY}&with_genres=99`
+
+//movies
+export const ACTION_MOVIES = `discover/movie?api_key=${API_KEY}&with_genres=28`
+export const COMEDY_MOVIES = `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const HORROR_MOVIES =`discover/movie?api_key=${API_KEY}&with_genres=27`
+export const ROMANCE_MOVIES = `discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const DOCUMENTARIES_MOVIES =`discover/movie?api_key=${API_KEY}&with_genres=99`
+export const SCIFI_MOVIES =`discover/movie?api_key=${API_KEY}&with_genres=878`
+export const THRILLER_MOVIES =`discover/movie?api_key=${API_KEY}&with_genres=53`
+
+//sereis
+export const ACTION_ADVENTURE_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=10759`
+export const CRIME_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=80`
+export const ANIMATION_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=16`
+export const COMEDY_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=35`
+export const KIDS_SERIES =`discover/tv?api_key=${API_KEY}&with_genres=10762`
+export const MYSTERY_SERIES =`discover/tv?api_key=${API_KEY}&with_genres=9648`
+export const ROMANCE_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=10749`
+export const SCIFI_SERIES = `discover/tv?api_key=${API_KEY}&with_genres=10765`
+export const TALK = `discover/tv?api_key=${API_KEY}&with_genres=10767`
+export const DOCUMENTARIES_SERIES =`discover/tv?api_key=${API_KEY}&with_genres=99`
